f30456bc7ddf06b4ebae1b228967b06d63f29161
Paweł Tetke <ptetke@gmail.com>
2017-08-12 16:02:53 +0200

    dodanie paczek

---
+++ ComputerShop.Core/Domain/User.cs
@@ -0,0 +1,11 @@
+﻿namespace ComputerShop.Core.Domain
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int PhoneNumber { get; set; }
+    }
+}
